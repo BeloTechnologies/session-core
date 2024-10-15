73333da990989b301845de7dfd5d3bdccfd36387
Turner Naef <Turner.Naef@gmail.com>
2024-10-15 17:03:29 -0600

    Drop id from incoming json model

--- core_models/user_service/create_user.go
+++ core_models/user_service/create_user.go
@@ -1,7 +1,6 @@
 package user_service
 
 type CreateUserEntry struct {
-	ID       int    `json:"id"`
 	Username string `json:"username"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
