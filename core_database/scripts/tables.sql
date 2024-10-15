dd0fe33f288a28efed0380e586c820d15d72fbdb
Turner Naef <Turner.Naef@gmail.com>
2024-10-15 17:51:13 -0600

    Split name and add phone no

--- core_database/scripts/tables.sql
+++ core_database/scripts/tables.sql
@@ -1,11 +1,13 @@
 -- tables.sql
 CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username VARCHAR(100) UNIQUE,
-                       name VARCHAR(100),
-                       email VARCHAR(100) UNIQUE,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    phone VARCHAR(100) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-                       followers_count INT DEFAULT 0,
-                       following_count INT DEFAULT 0,
+    followers_count INT DEFAULT 0,
+    following_count INT DEFAULT 0,
 );
--- core_models/user_service/create_user.go
+++ core_models/user_service/create_user.go
@@ -1,9 +1,11 @@
 package user_service
 
 type CreateUserEntry struct {
-	Username string `json:"username"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
+	Username  string `json:"username"`
+	FirstName string `json:"first-name"`
+	LastName  string `json:"last-name"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
 }
 
 type CreateUserEntryResponse struct {
